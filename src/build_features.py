98822d011d10a743acb3d0686688b96e704031da
vedika022 <gurav.vedika22@gmail.com>
2026-09-01 19:24:28 +0530

    Save processed customer data

---
+++ src/build_features.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+RAW_DATA_PATH = ROOT_DIR/ 'Data'/ 'Customers.csv'
+PROCESSED_DATA_PATH = ROOT_DIR / "Data" / "customers_processed.csv"
+
+df = pd.read_csv(RAW_DATA_PATH)
+
+df.fillna({'Profession': df['Profession'].mode()[0] }, inplace=True)
+
+features = df[['Annual Income ($)', 'Spending Score (1-100)']]
+scaler = MinMaxScaler()
+features_norm = scaler.fit_transform(features)
+features_norm = pd.DataFrame(features_norm, columns=features.columns)
+
+# Save processed data
+PROCESSED_DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
+features_norm.to_csv(PROCESSED_DATA_PATH, index=False)
+
